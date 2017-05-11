68ec8ba1bcbcd4a1805225646a820c8adfafa0e7
Max Keller <keller6@hm.edu>
2017-05-11 14:21:45 +0200

    Added duration.

--- app.py
+++ app.py
@@ -149,8 +149,8 @@ def update():
     succ = request.form.get('success', None)
     sentiment = request.form.get('sentiment', None)
     id = request.form["post_id"]
-    # duration = datetime.now() - datetime.strptime(request.form["work_time"], "%a %b %d %H:%M:%S %Y")
-    # print(duration)
+    duration_seconds = (
+        datetime.now() - datetime.strptime(request.form["work_time"], "%Y-%m-%d %H:%M:%S.%f")).total_seconds()
     error = 0
 
     # Validation checks
@@ -168,13 +168,13 @@ def update():
         return redirect(url_for('getpost', post_id=id))
 
     # Build statements
-    stmt = "REPLACE INTO category(user, post_id, category_name_id, sentiment, successful) VALUES(%s, %s, %s, %s, %s)"
+    stmt = "REPLACE INTO category(user, post_id, category_name_id, sentiment, successful, duration_seconds) VALUES(%s, %s, %s, %s, %s, %s)"
 
     # Update Record in Database
     print('Updating record ' + str(id))
     connection = get_db_connection()
     cursor = connection.cursor(buffered=True)
-    cursor.execute(stmt, (auth.username(), id, cat, sentiment, succ))
+    cursor.execute(stmt, (auth.username(), id, cat, sentiment, succ, duration_seconds))
     connection.commit()
     connection.close()
 
